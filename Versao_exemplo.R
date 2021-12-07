c3b2dd29756676ff24658afdccb4f1d7e4402eab
Henrique Akio Koga <akiokoga@hotmail.com>
2021-12-06 21:31:05 -0300

    Adicionando texto do documento GIT

--- Versao_exemplo.R
+++ Versao_exemplo.R
@@ -1,1 +1,15 @@
 # Escrevendo a primeira linha 
+
+# git init -> inicializa o Github
+
+# git add "nome do arquivo"
+
+# APENAS USAR ESSE STEP CASO NÃO TENHA CONFIGURADO 
+# git config --global user.email "email@hotmail.com"
+# git config --global user.name "Seu nome"
+
+# git commit -m "texto do commit"
+
+# git remote add origin https://github.com/akiokoga/MBA-USP.git
+
+# git push -u origin master
